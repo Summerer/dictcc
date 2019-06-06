bcad7f2c7d54a74721ccd2a3985bbd6e9f679ddd
Tim Summerer <summerer.tim@gmail.com>
2019-06-06 20:39:32 +0200

    print response to console on button press

--- app.py
+++ app.py
@@ -7,9 +7,31 @@
 # WARNING! All changes made in this file will be lost!
 
 from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QMessageBox
+
 import sys
+import tabulate
+
+import dictcc
+
 
 class Ui_MainWindow(object):
+
+    def on_click(self):
+        popup = QMessageBox()
+        popup.setIcon(QMessageBox.Information)
+        popup.setText('Translating...')
+        popup.setWindowTitle("Status Change")
+        popup.exec()
+        mytext = self.textEdit.toPlainText()
+        userInput = mytext.split()
+        response = dictcc.request(mytext, 'de', 'en')
+        data = dictcc.parse_response(response)
+        print(tabulate.tabulate(data, ['de', 'en'], tablefmt='orgtbl'))
+        
+
+
+
     def setupUi(self, MainWindow):
         MainWindow.setObjectName("MainWindow")
         MainWindow.resize(800, 600)
@@ -18,9 +40,13 @@ class Ui_MainWindow(object):
         self.pushButton = QtWidgets.QPushButton(self.centralwidget)
         self.pushButton.setGeometry(QtCore.QRect(690, 20, 88, 27))
         self.pushButton.setObjectName("pushButton")
-        self.tableView = QtWidgets.QTableView(self.centralwidget)
-        self.tableView.setGeometry(QtCore.QRect(10, 60, 771, 501))
-        self.tableView.setObjectName("tableView")
+
+        self.pushButton.setToolTip('Translate now')
+
+        #self.tableView = QtWidgets.QTableView(self.centralwidget)
+        #self.tableView.setGeometry(QtCore.QRect(10, 60, 771, 501))
+        #self.tableView.setObjectName("tableView")
+
         self.textEdit = QtWidgets.QTextEdit(self.centralwidget)
         self.textEdit.setGeometry(QtCore.QRect(210, 10, 471, 41))
         self.textEdit.setObjectName("textEdit")
@@ -42,10 +68,12 @@ class Ui_MainWindow(object):
         self.retranslateUi(MainWindow)
         QtCore.QMetaObject.connectSlotsByName(MainWindow)
 
+        self.pushButton.clicked.connect(self.on_click)
+
     def retranslateUi(self, MainWindow):
         _translate = QtCore.QCoreApplication.translate
         MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
-        self.pushButton.setText(_translate("MainWindow", "PushButton"))
+        self.pushButton.setText(_translate("MainWindow", "Translate"))
 
 
 class ApplicationWindow(QtWidgets.QMainWindow):
@@ -64,3 +92,5 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+
